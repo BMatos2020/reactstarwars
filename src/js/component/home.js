8e22b4f65436b6090bac971204ad22d78ef9d5a7
Brenda Matos <brenda.matosux@gmail.com>
2020-02-15 00:54:43 +0000

    map practice

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { Card } from "./card";
 
-//create your first component
+// export function Home() {
+// 	const [people, setPeople] = useState([]);
+
+// 	useEffect(() => {
+// 		fetch("https://swapi.co/api/people/")
+// 			.then(resp => resp.json())
+// 			.then(data => {
+// 				setPeople(data);
+// 				console.log(data);
+// 			});
+// 	}, []);
+
+// 	return (
+// 		<>
+// 			{people.map((item, index) => {
+// 				return <div key={index} />;
+// 			})}
+// 		</>
+// 	);
+// }
+
 export function Home() {
-	const [people, setPeople] = useState([]);
+	const [people, setPeople] = useState([
+		"Angel",
+		"Brenda",
+		"Joivre",
+		"Nick",
+		"Hernan",
+		"Ben"
+	]);
 
-	useEffect(() => {
-		fetch("https://swapi.co/api/films/")
-			.then(resp => resp.json())
-			.then(data => {
-				console.log(data);
-			});
-	}, []);
-
-	return <></>;
+	return (
+		<>
+			{people.map((item, index) => {
+				return <div key={index}>{item}</div>;
+			})}
+		</>
+	);
 }
