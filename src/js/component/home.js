89715a397b4b684afc62164321a48ed476cbfbf8
Brenda Matos <brenda.matosux@gmail.com>
2020-02-20 01:10:50 +0000

    map api using functional components

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,40 +1,72 @@
 import React, { useState, useEffect } from "react";
 import { Card } from "./card";
 
-// export function Home() {
-// 	const [people, setPeople] = useState([]);
+export function Home() {
+	const [people, setPeople] = useState([]);
+
+	useEffect(() => {
+		fetch("https://swapi.co/api/people/")
+			.then(resp => resp.json())
+			.then(data => {
+				setPeople(data);
+				// useState([]);
+				// console.log(data);
+			});
+	}, []);
+
+	return (
+		<div>
+			{people.results &&
+				people.results.map((item, index) => {
+					return <Card key={index} cardTitle={item.name} />;
+				})}
+		</div>
+	);
+}
+
+// On line 19 --- before mapping ,
+// REACT requires you to check whether there are items to display on load,
+// then it will render the map
 
 // 	useEffect(() => {
 // 		fetch("https://swapi.co/api/people/")
 // 			.then(resp => resp.json())
 // 			.then(data => {
-// 				setPeople(data);
+// 				setPeople(data.results);
+// 				// useState([]);
+// 				console.log(data);
+// 			});
+// 	}, []);
+
+// 	return (
+// 		<div>
+// 			{people.map((item, index) => {
+// 				return <Card key={index} cardTitle={item.name} />;
+// 			})}
+// 		</div>
+// 	);
+// }
+
+//
+
+// export function Home() {
+// 	const [people, setPeople] = useState([]);
+
+// 	useEffect(() => {
+// 		fetch("https://swapi.co/api/people/", {
+// 			method: "GET"
+// 		})
+// 			.then(resp => resp.json())
+// 			.then(data => {
+// 				useState([]);
+
 // 				console.log(data);
 // 			});
 // 	}, []);
 
 // 	return (
 // 		<>
-// 			{people.map((item, index) => {
-// 				return <div key={index} />;
-// 			})}
-// 		</>
-// 	);
-// }
-
-// export function Home() {
-// 	const [people, setPeople] = useState([
-// 		"Angel",
-// 		"Brenda",
-// 		"Joivre",
-// 		"Nick",
-// 		"Hernan",
-// 		"Ben"
-// 	]);
-
-// 	return (
-// 		<>
-// 			{people.map((item, index) => {
+// 			{people.results.map((item, index) => {
 // 				return <Card key={index} cardTitle={item} />;
 // 			})}
 // 			;
@@ -42,28 +74,22 @@ import { Card } from "./card";
 // 	);
 // }
 
-export function Home() {
-	const [people, setPeople] = useState([]);
+// // export function Home() {
+// // 	const [people, setPeople] = useState([
+// // 		"Angel",
+// // 		"Brenda",
+// // 		"Joivre",
+// // 		"Nick",
+// // 		"Hernan",
+// // 		"Ben"
+// // 	]);
 
-	useEffect(() => {
-		fetch("https://swapi.co/api/people/", {
-			method: "GET"
-		})
-			.then(resp => resp.json())
-			.then(data => {
-				people.map((item, index) => {
-					return <Card key={index} cardTitle={item} />;
-				});
-				console.log(data);
-			});
-	}, []);
-
-	// return (
-	// 	<>
-	// 		{people.map((item, index) => {
-	// 			return <Card key={index} cardTitle={item} />;
-	// 		})}
-	// 		;
-	// 	</>
-	// );
-}
+// // 	return (
+// // 		<>
+// // 			{people.map((item, index) => {
+// // 				return <Card key={index} cardTitle={item} />;
+// // 			})}
+// // 			;
+// // 		</>
+// // 	);
+// // }
